7aa0be6ba63ec580f4eda09a49f13b74cfe893bc
kys3923 <kys3923@gmail.com>
2021-03-20 03:13:30 -0400

    added about page

---
+++ components/landing/about.js
@@ -0,0 +1,19 @@
+const About = (props) => {
+  return (
+    <section>
+      <div>
+        <h3>About Me</h3>
+        <p>A full-stack web developer with a background in business management and graphic design. Expertise in graphic design includes producing, branding, and managing with corporate identities, vector/pixel graphics, photo editing, UI/UX design, printed/web deployed materials, and advertisement campaigns. Proficient in delivering visual languages as well as logical mapping in developments.</p>
+      </div>
+      <div>
+        <ul>
+        <li><a href="mailto:kys3923@gmail.com"><i className="far fa-envelope"></i> mail me</a></li>
+        <li><a href="https://drive.google.com/file/d/1ludrrUAsFFopnH-OxWjIMLWrGdu_Vb29/view?usp=sharing" target="_blank"><i className="fas fa-file-alt"></i> view resume</a></li>
+        <li><a href="https://www.linkedin.com/in/youngsang-kim/" target="_blank"><i className="fab fa-linkedin"></i> linkedIn</a></li>
+        <li><a href="https://github.com/kys3923" target="_blank"><i className="fab fa-github"></i> github</a></li>
+        </ul>
+      </div>
+    </section>
+  );
+}
+export default About;
--- pages/index.js
+++ pages/index.js
@@ -3,8 +3,10 @@ import Head from 'next/head'
 // importing pages
 import Welcome from '../components/landing/welcome'
 import Statement from '../components/landing/statement'
+import About from '../components/landing/about'
 
-export default function Home() {
+export default function Home({ desWorks, devWorks }) {
+
   return (
     <div>
       <Head>
@@ -14,7 +16,39 @@ export default function Home() {
       <Welcome />
       <Statement />
       <h3>Recent Dev works</h3>
+      {devWorks &&
+        devWorks.map((devWork) => (
+          <div key={devWork.id}>
+            <h3>{devWork.name}</h3>
+            {/* TODO: add card to display */}
+          </div>
+        ))
+      }
       <h3>Recent Design works</h3>
+      {desWorks &&
+        desWorks.map((desWork) => (
+          <div key={desWork.id}>
+            <h3>{desWork.name}</h3>
+            {/* TODO: add card to display */}
+          </div>
+        ))
+      }
+      <About />
     </div>
   )
+}
+
+
+export async function getStaticProps() {
+
+  // get posts from the api
+  const resDesign = await fetch('http://localhost:1337/design-works');
+  const resDev = await fetch('http://localhost:1337/dev-works');
+  const desWorks = await resDesign.json();
+  const devWorks = await resDev.json();
+
+  return {
+    props: { desWorks, devWorks },
+  };
+
 }
