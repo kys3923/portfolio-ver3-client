8b93cd4527bebd95c9615b8a66960d3354501a8e
kys3923 <kys3923@gmail.com>
2021-03-23 09:09:32 -0400

    footer added

--- components/footer.js
+++ components/footer.js
@@ -1,10 +1,19 @@
 const Footer = (props) => {
   return (
-    <div>
-      <h3>Footer</h3>
-      <ul>
-        <li><i class="fab fa-linkedin-in"></i></li>
-      </ul>
+    <div className="footer-container">
+      <div className="footer-YK">
+        <h3>YK | Portfolio</h3>
+      </div>
+      <div className="footer-lists">
+        <ul>
+          <li><a href="https://drive.google.com/file/d/1ludrrUAsFFopnH-OxWjIMLWrGdu_Vb29/view?usp=sharing" target="_blank"><i className="fas fa-file-alt"></i> &nbsp; View Resume</a></li>
+          <li><a href="https://www.linkedin.com/in/youngsang-kim/" target="_blank"><i className="fab fa-linkedin-in"></i> &nbsp; LinkedIn</a></li>
+          <li><a href="https://github.com/kys3923" target="_blank"><i className="fab fa-github"></i> &nbsp; Github</a></li>
+        </ul>
+      </div>
+      <div className="footer-copy">
+        <p>Made with ♥ & Next.js & hard code CSS, <span id="copyright">©</span> YK, 2021</p>
+      </div>
     </div>
   );
 }
