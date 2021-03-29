2b65b1b7e96096058934b123f12dcce38c7a87be
kys3923 <kys3923@gmail.com>
2021-03-28 23:56:32 -0400

    updated detail page

--- pages/dev/[name].js
+++ pages/dev/[name].js
@@ -1,44 +1,116 @@
 import Head from 'next/head'
 import { API_URL } from '../../utils/urls'
+import { FormattedDate, IntlProvider } from 'react-intl'
 
+import { makeStyles } from '@material-ui/core/styles'
+import {
+  Grid,
+  Paper,
+} from '@material-ui/core'
 
+import { motion } from 'framer-motion'
+
+const useStyles = makeStyles((theme) => ({
+  root: {
+    marginTop: '4vh',
+    flexGrow: 1,
+    padding: '3em',
+    backgroundColor: 'rgb(246, 243, 240)'
+  },
+  title: {
+    padding: '6em',
+    paddingLeft: '4em',
+    backgroundColor: 'rgb(246, 243, 240)'
+
+  },
+  titleText: {
+    fontSize: '10vw',
+    letterSpacing: '-0.5vw',
+  },
+  imageCard: {
+    width: '100%',
+    marginBottom: '3vh',
+    margin: 'auto'
+  },
+  image: {
+    width: '100%',
+    objectFit: 'cover',
+  },
+  cardContainer: {
+    display: 'flex',
+    flexDirection: 'column',
+    justifyContent: 'center',
+  },
+  desCard: {
+    backgroundColor: 'rgb(246, 243, 240)',
+    paddingLeft: '2em',
+    paddingRight: '3em',
+  },
+  skill: {
+    backgroundColor: 'white',
+    display: 'inline-block',
+    padding: '5px',
+    marginRight: '5px',
+    marginBottom: '5px',
+    borderRadius: '10px'
+  }
+}))
 
 export default function DevWork({ work }) {
+
+  const classes = useStyles()
+
+  let newYear = (
+    <IntlProvider locale='en' defaultLocale="en">
+        <FormattedDate value={work.created_at} month='2-digit' year='numeric' />
+    </IntlProvider>
+  )
+
   return (
     <div>
       <Head>
         <title>YK | Dev</title>
         <link rel="icon" href="/img/favicon.ico" />
         <link rel="preconnect" href="https://fonts.gstatic.com" />
-        <link href="https://fonts.googleapis.com/css2?family=Nanum+Gothic:wght@400;700;800&display=swap" rel="stylesheet"></link>
         <meta name="viewport" content="minimum-scale=1, initial-scale=1, width=device-width" />
       </ Head>
-      <div className="dev-page-container2">
-        <div className="dev-page-intro">
-          <h3>{work.name}.</h3>
-        </div>
-        <div className="dev-page-detail">
-          <h3>Project Name: {work.name}</h3>
-          <h5>{work.type}</h5>
-          <ul>
-            <p>Tech used:</p>
-            {work.tech.map((skill) => (
-              <span key={skill.id}>{skill.tech} &nbsp; </span>
-            ))}
-          </ul>
-          <a href={work.link} target="_blank">view live here</a>
-        </div>
-        <div className="dev-page-image">
-          {work.images.map((image) => (
-            <div key={image.id} className="page-image">
-              <img src={image.image} />
-            </div>
-          ))}
-        </div>
-        <div className="dev-page-detail2">
-          <p>{work.task}</p>
-          <p>{work.solution}</p>
-        </div>
+      <div className={classes.root}>
+        <Grid container>
+          <motion.div
+            key="designPageDetailJS"
+            initial={{ opacity: 0 }}
+            animate={{ opacity: 1 }}
+            exit={{ opacity:0 }}
+          >
+            <Grid item xs={12}>
+              <Paper className={classes.title} elevation={0}>
+                <h3 className={classes.titleText}>{work.name}</h3>
+              </Paper>
+            </Grid>
+            <Grid item xs={12} sm={10} className={classes.cardContainer}>
+              {work.images.map((image) => (
+                <Paper key={image.id} className={classes.imageCard}>
+                  <img src={image.image} className={classes.image} />
+                </Paper>
+              ))}
+            </Grid>
+            <Grid item xs={12} sm={10}>
+              <Paper className={classes.desCard} elevation={0}>
+                <h1>{work.name}</h1>
+                <h4>{work.type}</h4>
+                <h5>{newYear}</h5>
+                <p>
+                  {work.tech.map((skill) => (
+                    <span key={skill.id} className={classes.skill}>{skill.tech}</span>
+                  ))}
+                </p>
+                <p>{work.task}</p>
+                <p>{work.solution}</p>
+                <p className="work_link"><a href={work.link} target="_blank" className="work-link">Live View</a></p>
+              </Paper>
+            </Grid>
+          </motion.div>
+        </Grid>
       </div>
     </div>
   )
